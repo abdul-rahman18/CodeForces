a30269d0f534d74f4c8babac56e93ec81ed0a1fa
Abdul Rahman <mohammedabdulrahman188@gmail.com>
2026-07-08 15:40:19 +0530

    Add Ambitious Kid [A] from Codeforces

---
+++ 1866/A - Ambitious Kid/solution.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    int n; cin >> n;
+    
+    int mn = INT_MAX;
+    for(int i=0;i<n;i++)
+    {
+        int x; cin >> x;
+        
+        x = abs(x);
+        
+        mn = min(mn,x);
+    }
+    
+    cout << mn << endl;
+}
