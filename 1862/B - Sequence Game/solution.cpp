6e472898bb0c00419da6cc49d81dcc2286333be5
Abdul Rahman <mohammedabdulrahman188@gmail.com>
2026-07-09 14:17:22 +0530

    Add Sequence Game [B] from Codeforces

--- 1862/B - Sequence Game/solution.cpp
+++ 1862/B - Sequence Game/solution.cpp
@@ -9,21 +9,19 @@ int main()
     {
         int n; cin >> n;
         
-        vector<int>a(n);
-        for(int i=0;i<n;i++) cin >> a[i];
-        
         vector<int>ans;
-        ans.push_back(a[0]);
-        
-        for(int i=1;i<n;i++) 
+        for(int i=0;i<n;i++)
         {
-            if(a[i-1] > a[i]) ans.push_back(a[i]);
-            ans.push_back(a[i]);
+            int x; cin >> x;
+            
+            if(i>0 && ans.back()>x) ans.push_back(x);
+            
+            ans.push_back(x);
         }
         
         cout << ans.size() << endl;
-        
         for(int i=0;i<ans.size();i++) cout << ans[i] << " ";
+        
         cout << endl;
     }
     
